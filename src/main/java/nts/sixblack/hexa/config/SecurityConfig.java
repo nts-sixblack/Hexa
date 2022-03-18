dd000e11e54edf6545a0f61a7b9c418e4f8eed1a
nts-sixblack <nts.sixblack@gmail.com>
2022-03-18 18:52:54 +0700

    security config

--- src/main/java/nts/sixblack/hexa/config/SecurityConfig.java
+++ src/main/java/nts/sixblack/hexa/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers("/user/**")
                 .permitAll()
-                .antMatchers("/user/login")
+                .anyRequest()
                 .authenticated();
 
         http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
